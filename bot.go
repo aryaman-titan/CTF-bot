ee4c511a7b910c5b9bc405e4d9b74858057ff6a7
aryaman-titan <aryamanbehera@gmail.com>
2020-07-08 19:57:45 +0530

    first commit

---
+++ bot.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: mybot slack-bot-token\n")
+		os.Exit(1)
+	}
+
+	// start a websocket-based Real Time API session
+	ws, id := slackConnect(os.Args[1])
+	fmt.Println("mybot ready, ^C exits")
+
+	for {
+		// read each incoming message
+		m, err := getMessage(ws)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// see if we're mentioned
+		if m.Type == "message" && strings.HasPrefix(m.Text, "<@"+id+">") {
+			// if so try to parse if
+			parts := strings.Fields(m.Text)
+			if len(parts) == 3 && parts[1] == "stock" {
+				// looks good, get the quote and reply with the result
+				go func(m Message) {
+					m.Text = getQuote(parts[2])
+					postMessage(ws, m)
+				}(m)
+				// NOTE: the Message object is copied, this is intentional
+			} else {
+				// huh?
+				m.Text = fmt.Sprintf("sorry, that does not compute\n")
+				postMessage(ws, m)
+			}
+		}
+	}
+}
+
+// Get the quote via Yahoo. You should replace this method to something
+// relevant to your team!
+func getQuote(sym string) string {
+	sym = strings.ToUpper(sym)
+	url := fmt.Sprintf("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nsl1op&e=.csv", sym)
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+	rows, err := csv.NewReader(resp.Body).ReadAll()
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+	if len(rows) >= 1 && len(rows[0]) == 5 {
+		return fmt.Sprintf("%s (%s) is trading at $%s", rows[0][0], rows[0][1], rows[0][2])
+	}
+	return fmt.Sprintf("unknown response format (symbol was \"%s\")", sym)
+}
---
+++ slack.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync/atomic"
+
+	"golang.org/x/net/websocket"
+)
+
+// These two structures represent the response of the Slack API rtm.start.
+// Only some fields are included. The rest are ignored by json.Unmarshal.
+
+type responseRtmStart struct {
+	Ok    bool         `json:"ok"`
+	Error string       `json:"error"`
+	URL   string       `json:"url"`
+	Self  responseSelf `json:"self"`
+}
+
+type responseSelf struct {
+	ID string `json:"id"`
+}
+
+// slackStart does a rtm.start, and returns a websocket URL and user ID. The
+// websocket URL can be used to initiate an RTM session.
+func slackStart(token string) (wsurl, id string, err error) {
+	url := fmt.Sprintf("https://slack.com/api/rtm.start?token=%s", token)
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("API request failed with code %d", resp.StatusCode)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+	var respObj responseRtmStart
+	err = json.Unmarshal(body, &respObj)
+	if err != nil {
+		return
+	}
+
+	if !respObj.Ok {
+		err = fmt.Errorf("Slack error: %s", respObj.Error)
+		return
+	}
+
+	wsurl = respObj.URL
+	id = respObj.Self.ID
+	return
+}
+
+//Message These are the messages read off and written into the websocket. Since this
+// struct serves as both read and write, we include the "Id" field which is
+// required only for writing.
+type Message struct {
+	ID      uint64 `json:"id"`
+	Type    string `json:"type"`
+	Channel string `json:"channel"`
+	Text    string `json:"text"`
+}
+
+func getMessage(ws *websocket.Conn) (m Message, err error) {
+	err = websocket.JSON.Receive(ws, &m)
+	return
+}
+
+var counter uint64
+
+func postMessage(ws *websocket.Conn, m Message) error {
+	m.ID = atomic.AddUint64(&counter, 1)
+	return websocket.JSON.Send(ws, m)
+}
+
+// Starts a websocket-based Real Time API session and return the websocket
+// and the ID of the (bot-)user whom the token belongs to.
+func slackConnect(token string) (*websocket.Conn, string) {
+	wsurl, id, err := slackStart(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ws, err := websocket.Dial(wsurl, "", "https://api.slack.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ws, id
+}
